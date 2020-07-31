5bcf87bcee522f49681b7d9e22e5a7affed573dd
gilvan <g.r.almeida@live.com>
2020-07-31 13:05:42 -0300

    Tipos das variáveis

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -3,5 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Olá Mundo")
+	var nome string = "Douglas"
+	var versao float32 = 1.1
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
 }
