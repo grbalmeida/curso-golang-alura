3620641f57d58fe7506dbac4ec77e5c6f22f6208
gilvan <g.r.almeida@live.com>
2020-08-01 21:25:19 -0300

    Inferência de tipos

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var nome string = "Douglas"
-	var versao float32 = 1.1
+	nome := "Douglas"
+	versao := 1.1
 	fmt.Println("Olá, sr.", nome)
 	fmt.Println("Este programa está na versão", versao)
 }
